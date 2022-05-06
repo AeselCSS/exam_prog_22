60c4e4097808b00d49d8ea6651650e0e40d0b824
AeselCSS <chso21ad@student.cbs.dk>
2022-05-06 20:37:49 +0200

    user functionality up and running

---
+++ app/modules/users/user_delete.js
@@ -0,0 +1,25 @@
+document.getElementById("delete-user-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+  
+    const user = JSON.parse(localStorage.getItem("user"));
+    let url = `http://localhost:3000/users/${user[0].id}`;
+  
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response) {
+            window.alert(`user deleted. - You will be redirected to the login page`)
+            localStorage.removeItem("user");
+            location.href = "../../../client/login.html";
+        }
+      })
+      .catch(() => {
+        window.alert("oh noes! - Something went wrong.");
+      });
+  });
--- app/modules/users/user_login.js
+++ app/modules/users/user_login.js
@@ -1,3 +1,10 @@
+// CHECK IF USER IS ALREADY LOGGED IN
+document.addEventListener("DOMContentLoaded", (e) => {
+  const user = localStorage.getItem("user");
+  if (user) { // IF SO: REDIRECT TO INDEX.HTML
+    location.href = "../../../client/index.html";
+  }
+
 // USER LOGIN
 
     const userLogin = document.getElementById("loginForm")
@@ -24,13 +31,12 @@
     .then(response => response.json())
     .then(data => {
       if (data.length === 0) {
-        window.alert("WRGONG!!!!")
+        window.alert("GET TOO THE CHOPPAAR!!!!")
       } else {
-        window.alert("Du er nu logget ind")
         localStorage.setItem('user',JSON.stringify(data));
         location.href = "../../../client/index.html";
     }
   })
 });
-
+});
 
